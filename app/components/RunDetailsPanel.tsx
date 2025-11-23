115cb49ba95d4e4a05d5cf559ebd280e94b70bc7
Tristan Arlot <tristan45220@hotmail.fr>
2025-11-23 20:10:12 +0100

    minor fix

--- app/components/RunDetailsPanel.tsx
+++ app/components/RunDetailsPanel.tsx
@@ -11,7 +11,7 @@ interface RunDetailsPanelProps {
 
 export default function RunDetailsPanel({ run }: RunDetailsPanelProps) {
     return (
-        <div className="w-fit rounded-2xl bg-gray-900 mb-4">
+        <div className="w-fit mx-4 rounded-2xl bg-gray-900 mb-4">
             <div className="flex flex-col sm:flex-row items-start sm:items-center h-92 sm:h-82 lg:h-40 xl:h-50 px-4 py-3 gap-4">
                 {/* Ligne du haut sur très petits écrans: Stats + Badges */}
                 <div className="flex flex-row items-start w-full sm:w-auto gap-3 h-full sm:gap-4">
--- app/page.tsx
+++ app/page.tsx
@@ -201,10 +201,10 @@ export default function Home() {
                     onSelectRun={handleSelectRun}
                 />
 
-                <div className="flex-1 flex flex-col justify-center max-w-full px-4 pt-4 pb-2">
+                <div className="flex-1 flex flex-col justify-center max-w-full  pt-4 pb-2">
                     <div className="w-full flex flex-col justify-center ">
                         {/* Bloc mention du doc */}
-                        <div className="mb-4 w-fit px-4 py-2 bg-gray-800/60 backdrop-blur-sm border border-gray-600/50 rounded-lg shadow-lg">
+                        <div className="mb-4 w-fit mx-4 px-4 py-2 bg-gray-800/60 backdrop-blur-sm border border-gray-600/50 rounded-lg shadow-lg">
                             <p className="text-center text-xs sm:text-base text-gray-300">
                                 D&apos;après le{' '}
                                 <a
@@ -221,7 +221,7 @@ export default function Home() {
 
                         {/* Rencontres - scroll horizontal */}
                         <HorizontalScrollContainer>
-                            <div className="inline-flex gap-4 pb-4 sm:gap-7 min-w-max">
+                            <div className="inline-flex mx-4 gap-4 pb-4 sm:gap-7 min-w-max">
                                 {selectedRun.team
                                     .sort(
                                         (a, b) =>
